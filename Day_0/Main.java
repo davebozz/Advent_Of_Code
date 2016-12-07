282e1b7df88ac41fa7bee96e5942cfb947cd4398
DJ Boz <djbozentka@gmail.com>
2016-12-07 18:44:03 -0500

    inital commit

---
+++ Day_0/Main.java
@@ -0,0 +1,76 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Declare ArrayLists for moves
+		ArrayList<String> letterMoves = new ArrayList<String>();
+		ArrayList<Integer> numMoves = new ArrayList<Integer>();
+		
+		initAndRead(letterMoves, numMoves);
+		
+		//Start new Traveler
+		Rambler rambler = new Rambler();
+		System.out.println(letterMoves.size());
+		System.out.println(numMoves.size());
+		for(int i = 0; i < letterMoves.size(); i++) {
+			rambler.newDirection(letterMoves.get(i));
+			rambler.move(numMoves.get(i));
+			
+			System.out.println("Move: "+ letterMoves.get(i) + "" + numMoves.get(i));
+			System.out.println("Cur Direction: " + rambler.getDirection());
+			System.out.println("Current X Position : " + rambler.getX());
+			System.out.println("Current Y Position : " + rambler.getY());
+			System.out.println("");
+		}
+		
+		System.out.println(rambler.getX());
+		System.out.println(rambler.getY());
+		System.out.println(rambler.getDirection());
+		int ans = Math.abs(rambler.getX()) + Math.abs(rambler.getY());
+		System.out.println("Total Moves: " + ans);
+		
+	}
+	
+	public static void initAndRead(ArrayList<String> letter, ArrayList<Integer> numbers)
+	{
+		//File to be read in
+		File file = new File("input.txt");
+		
+		//ArrayList of Moves
+		ArrayList<String> moves = new ArrayList<String>();
+		
+		try {
+			Scanner scan = new Scanner(file);
+			
+			scan.useDelimiter(",|" + System.getProperty("line.seperator"));
+						
+			while(scan.hasNext()) {
+				moves.add(scan.next());
+			}
+			System.out.println("Total Moves: " + moves.size());
+			
+		} catch(FileNotFoundException e) {
+			System.out.println("file not found: " + file.toString());
+		}		
+		
+		for(String temp : moves) {
+			//Temp List
+			String[] tempArr = temp.trim().split("[LR]");
+			String[] tempArr2 = temp.trim().split("\\d");
+			
+			//Fill ArrayLists
+			letter.add(tempArr2[0]);
+			
+			//Better way to perform faster operation, than converting to a integer every time?
+			numbers.add(Integer.parseInt(tempArr[1]));
+
+		}		
+	}
+	
+}
+
---
+++ Day_0/Rambler.java
@@ -0,0 +1,87 @@
+public class Rambler {
+	
+	private int x = 0;
+	private int y = 0;
+	
+	private String direction = "N";
+	
+	
+	//Accessors & Mutators
+	protected int getX()
+	{
+		return this.x;
+	}
+	
+	protected int getY()
+	{
+		return this.y;
+	}
+	
+	protected String getDirection()
+	{
+		return this.direction;
+	}
+	
+	protected void setX(int x)
+	{
+		this.x = x;
+	}
+	
+	protected void setY(int y)
+	{
+		this.y = y;
+	}
+	
+	protected void setDirection(String direction)
+	{
+		this.direction = direction;
+	}
+	
+	protected void move(int numSpaces)
+	{
+		if( direction.equals("N") )
+			this.y += numSpaces;
+		else if ( direction.equals("S") )
+			this.y -= numSpaces;
+		else if ( direction.equals("E") )
+			this.x += numSpaces;
+		else if ( direction.equals("W"))
+			this.x -= numSpaces;
+	}
+	
+	protected void newDirection(String dir)
+	{
+		String curDir = "";
+		
+		if ( direction.equals("N") ) {
+			//Direction == N Left(=West) Right(=East)
+			if ( dir.equals("R") )
+				curDir = "E";
+			else 
+				curDir = "W";
+			
+		} else if ( direction.equals("E") ) {
+			//Direction == E Left(=North) Right(=South)
+			if ( dir.equals("R") )
+				curDir = "S";
+			else
+				curDir = "N";
+		} else if ( direction.equals("S") ) {
+			
+			//Direction == S Left(=East) Right(=West)
+			if ( dir.equals("R") )
+				curDir = "W";
+			else
+				curDir = "E";
+		} else if ( direction.equals("W") ) {
+			
+			//Direction == W Left(=South) Right(=North)
+		    if ( dir.equals("R") )
+		    	curDir = "N";
+		    else
+		    	curDir = "S";
+		}
+		
+		this.direction = curDir;
+	}
+}
